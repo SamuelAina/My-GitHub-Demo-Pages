7d9829ea4f71698d26aa0cb918007b2693e8764d
Samuel Aina <samuel.aina@gmail.com>
2023-05-02 19:46:37 +0100

    Update templateLoader.js

--- templateLoader.js
+++ templateLoader.js
@@ -3,6 +3,7 @@ function loadTemplate(templateName, targetElementId) {
         .then((response) => response.text())
         .then((html) => {
             document.getElementById(targetElementId).innerHTML = html;
+			//closeMenu();
         })
         .catch((error) => {
             console.warn('Something went wrong while loading the template:', error);
@@ -13,6 +14,26 @@ function loadTemplate(templateName, targetElementId) {
 
 window.addEventListener('DOMContentLoaded', () => {
     loadTemplate('header.html', 'header');
-    //loadTemplate('footer.html', 'footer');
+    loadTemplate('footer.html', 'footer');
 });
 
+window.onload = function(){
+	setTimeout(
+	    function(){
+			closeMenu();
+			document.getElementById("hamburger").addEventListener("click", openMenu);
+	}, 100)
+    //
+}
+
+function openMenu() {
+    document.getElementById("menu").style.left = "0";
+    document.getElementById("menu").style.width ="200px";
+    document.getElementById("menu").style.display="block";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.width = "200px";
+    document.getElementById("menu").style.left = "200px";
+    document.getElementById("menu").style.display="none"
+}
