0ae7e9ea4fe85f9a06ae57ccd65045d424c4eeb0
Samuel Aina <samuel.aina@gmail.com>
2024-05-25 09:27:06 +0100

    Add files via upload

---
+++ Jaaloo/PS/database.js
@@ -0,0 +1,276 @@
+originaldatabase= 
+{"modules" : [
+  {
+    "moduleID": "M1",
+    "moduleTitle": "User Registration and Profile Management",
+    "sortPosition": 1
+  },
+  {
+    "moduleID": "M2",
+    "moduleTitle": "Investment and Contribution Tracking",
+    "sortPosition": 2
+  },
+  {
+    "moduleID": "M3",
+    "moduleTitle": "Workstream Management and Incentivization",
+    "sortPosition": 3
+  },
+  {
+    "moduleID": "M4",
+    "moduleTitle": "Profit Sharing Calculation",
+    "sortPosition": 4
+  },
+  {
+    "moduleID": "M5",
+    "moduleTitle": "Financial Management",
+    "sortPosition": 5
+  },
+  {
+    "moduleID": "M6",
+    "moduleTitle": "Communication, Collaboration, and Marketing",
+    "sortPosition": 6
+  },
+  {
+    "moduleID": "M7",
+    "moduleTitle": "Educational Resources and Support",
+    "sortPosition": 7
+  },
+  {
+    "moduleID": "M8",
+    "moduleTitle": "Subscription and Packages Management",
+    "sortPosition": 8
+  },
+  {
+    "moduleID": "M9",
+    "moduleTitle": "Legal, Tax, and Affiliate Management",
+    "sortPosition": 9
+  },
+  {
+    "moduleID": "M10",
+    "moduleTitle": "Analytics, R&D, and Document Management",
+    "sortPosition": 10
+  }
+] ,
+
+"features":[
+  {
+    "featureID": "F1",
+    "moduleID": "M1",
+    "featureTitle": "Log in",
+    "sortPosition": 1,
+    "href": "Log-in.html"
+  },
+  {
+    "featureID": "F2",
+    "moduleID": "M1",
+    "featureTitle": "Manage User Info",
+    "sortPosition": 2,
+    "href": "Manage-User-Info.html"
+  },
+  {
+    "featureID": "F3",
+    "moduleID": "M1",
+    "featureTitle": "Manage User Permissions",
+    "sortPosition": 3,
+    "href": "User-profiles.html"
+  },
+  {
+    "featureID": "F4",
+    "moduleID": "M1",
+    "featureTitle": "Account Creation",
+    "sortPosition": 4,
+    "href": "Account-Creation.html"
+  },
+  {
+    "featureID": "F5",
+    "moduleID": "M1",
+    "featureTitle": "Reset Password",
+    "sortPosition": 5,
+    "href": "Reset-Password.html"
+  },
+  {
+    "featureID": "F6",
+    "moduleID": "M1",
+    "featureTitle": "Log out",
+    "sortPosition": 6,
+    "href": "#",
+    "onclick": "logout()"
+  },
+  {
+    "featureID": "F6-2",
+    "moduleID": "M1",
+    "featureTitle": "Reset Database (System Command)",
+    "sortPosition": 6,
+    "href": "#",
+    "onclick": "resetDB()"
+  },
+  {
+    "featureID": "F7",
+    "moduleID": "M2",
+    "featureTitle": "Monetary Investments",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F8",
+    "moduleID": "M2",
+    "featureTitle": "Time and Effort Tracking",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F9",
+    "moduleID": "M2",
+    "featureTitle": "Contribution Recording",
+    "sortPosition": 3,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F10",
+    "moduleID": "M3",
+    "featureTitle": "Workstream Database",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F11",
+    "moduleID": "M3",
+    "featureTitle": "Performance Metrics",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F12",
+    "moduleID": "M4",
+    "featureTitle": "Dynamic Calculations",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F13",
+    "moduleID": "M4",
+    "featureTitle": "Quarterly Adjustments",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F14",
+    "moduleID": "M5",
+    "featureTitle": "Transaction Recording",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F15",
+    "moduleID": "M5",
+    "featureTitle": "Financial Reporting",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F16",
+    "moduleID": "M6",
+    "featureTitle": "Built-in Communication Tools",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F17",
+    "moduleID": "M6",
+    "featureTitle": "Notifications and Alerts",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F18",
+    "moduleID": "M7",
+    "featureTitle": "Resource Library",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F19",
+    "moduleID": "M7",
+    "featureTitle": "Support Services",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F20",
+    "moduleID": "M8",
+    "featureTitle": "Package Options",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F21",
+    "moduleID": "M9",
+    "featureTitle": "Tax and Legal",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F22",
+    "moduleID": "M9",
+    "featureTitle": "Affiliate Marketing: Manage affiliate programs, track performance, and integrate commissions into the profit-sharing model.",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F23",
+    "moduleID": "M10",
+    "featureTitle": "Performance Analytics",
+    "sortPosition": 1,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F24",
+    "moduleID": "M10",
+    "featureTitle": "R&D Module",
+    "sortPosition": 2,
+    "href": "under-development.html"
+  },
+  {
+    "featureID": "F25",
+    "moduleID": "M10",
+    "featureTitle": "Document Management",
+    "sortPosition": 3,
+    "href": "under-development.html"
+  }
+] ,
+
+  "roles" : [{"roleID":"R1", roleTitle:"Investor"},{"roleID":"R2", roleTitle:"Programmer"},{"roleID":"R3", roleTitle:"Frelance"},{"roleID":"R4", roleTitle:"Superuser"}]
+,
+  "rolefeatures" : [{"roleID":"R4", "featureID": "F1"},{"roleID":"R4", "featureID": "F2"},{"roleID":"R4", "featureID": "F13"},{"roleID":"R4", "featureID": "F4", "featureID": "F5"}]
+,
+  "users" : [
+          {"username":"xx@xx.com", "password":"xxx", "fullname":"Joe Bloggs","email":"xx@xx.com","phone":"111","dob":"1970-01-01","occupation":"Teacher","bank":"aaaa","currency":"USD","role":"investor","loggedin":false},
+          {"username":"zz@zz.com", "password":"zzz", "fullname":"Fred Flop","email":"zz@zz.com","phone":"222","dob":"1970-02-02","occupation":"Student","bank":"bbbb","currency":"GBP","role":"investor","loggedin":false},
+          {"username":"yy@yy.com", "password":"yyy", "fullname":"Lucy Canon","email":"yy@yy.com","phone":"333","dob":"1970-03-03","occupation":"Student","bank":"cccc","currency":"USD","role":"investor","loggedin":false},
+          {"username":"aa@aa.com", "password":"aaa", "fullname":"Handy Mann","email":"aa@aa.com","phone":"444","dob":"1960-04-04","occupation":"Teacher","bank":"dddd","currency":"USD","role":"investor","loggedin":false},
+          {"username":"bb@bb.com", "password":"bbb", "fullname":"Sue Ben","email":"bb@bb.com","phone":"555","dob":"1960-05-05","occupation":"Programmer","bank":"eeee","currency":"GBP","role":"investor","loggedin":false}
+      ]    
+,
+
+  "userroles":[
+      {"username":"xx@xx.com", "roleID":"R4"}
+      ,{"username":"yy@yy.com", "roleID":"R4"}  
+      ],
+
+"activeUser": null
+};
+
+
+
+
+
+database=loadFromLocalStorage('DATABASE') ? loadFromLocalStorage('DATABASE'):originaldatabase
+
+
+
+
+
+function loadFromLocalStorage(key) {
+	const jsonString = localStorage.getItem(key.toUpperCase());
+	return JSON.parse(jsonString);
+}
